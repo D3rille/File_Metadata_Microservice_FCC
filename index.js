45f1e9794fb563f16223a0e6cc05a12bf4cd1e2d
Jhan Derille Unlayao <derillejhanunlayao@gmail.com>
2024-04-25 16:10:43 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: './public/uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req,res)=>{
+  try {
+    
+    console.log(req.body, req.file);
+    res.status(200).json(req.file);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
 
 
 
